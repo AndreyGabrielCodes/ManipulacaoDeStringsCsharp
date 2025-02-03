4f5f170743c54f8f2e3c4fd6e5be2edb84ba7358
Andrey Gabriel <andreygabriel.scnd@gmail.com>
2025-02-03 16:37:16 -0300

    CREATE | Finalizado Q1
    - Criado funções para contar vogais e consoantes
    - Criado atalho para o executavel do programa

---
+++ ManipulacaoDeStringsCsharp/ContaVogaisEConsoantes.cs
@@ -0,0 +1,59 @@
+﻿namespace Q1
+{
+    public class ContaVogaisEConsoantes
+    {
+        public static string VerificaOcorrencias(string texto)
+        {
+            return $"\"{texto}\" - Vogais: {ContaVogais(texto)} " +
+                $"| Consoantes: {ContaConsoantes(texto)}";
+        }
+
+        private static int ContaVogais(string texto)
+        {
+            int contagem = 0;
+            foreach (char letra in texto.ToLower())
+            {
+                switch (letra)
+                {
+                    case 'a':
+                        contagem++;
+                        break;
+                    case 'e':
+                        contagem++;
+                        break;
+                    case 'i':
+                        contagem++;
+                        break;
+                    case 'o':
+                        contagem++;
+                        break;
+                    case 'u':
+                        contagem++;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return contagem;
+        }
+
+        private static int ContaConsoantes(string texto)
+        {
+            int contagem = 0;
+            char[] consoantes = { 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k',
+                'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z' };
+
+            foreach (char letra in texto.ToLower())
+            {
+                foreach (char consoante in consoantes)
+                {
+                    if (letra == consoante)
+                    {
+                        contagem++;
+                    }
+                }
+            }
+            return contagem;
+        }
+    }
+}
--- ManipulacaoDeStringsCsharp/Program.cs
+++ ManipulacaoDeStringsCsharp/Program.cs
@@ -11,22 +11,24 @@ namespace ManipulacaoDeStringsCsharp
         static void Main(string[] args)
         {
             //Q1
+            string textoQ1 = "Eu quero uma pizza";
             Console.WriteLine("Q1 - Conte o número de vogais e consoantes a partir " +
-                "de uma string");
+                $"de uma string\n   - Input: {textoQ1}");
+            Console.WriteLine("   - " + Q1.ContaVogaisEConsoantes.VerificaOcorrencias(textoQ1));
 
             //Q2
-            Console.WriteLine("Q2 - Inverta a ordem dos caracteres de uma string");
+            Console.WriteLine("\nQ2 - Inverta a ordem dos caracteres de uma string");
 
             //Q3
-            Console.WriteLine(@"Q3 - Determine se uma string é um palíndromo " +
+            Console.WriteLine("\nQ3 - Determine se uma string é um palíndromo " +
                 "(se ela é igual quando lida da direita para esquerda e vice-versa)");
 
             //Q4
-            Console.WriteLine(@"Q4 - Determine se duas strings são anagramas " +
+            Console.WriteLine("\nQ4 - Determine se duas strings são anagramas " +
                 "(possuem os mesmos caracteres em ordens diferentes)");
 
             //Q5
-            Console.WriteLine("Q5 - Valide se a senha inserida atende aos requisitos");
+            Console.WriteLine("\nQ5 - Valide se a senha inserida atende aos requisitos");
 
         }
     }
