7ea9e226ae7f65a55dde839588b2028bdbfb8b19
Andrey Gabriel <andreygabriel.scnd@gmail.com>
2025-02-03 15:58:26 -0300

    CREATE | Iniciado projeto
    - Criado projeto com o texto das perguntas

---
+++ ManipulacaoDeStringsCsharp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManipulacaoDeStringsCsharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Q1
+            Console.WriteLine("Q1 - Conte o número de vogais e consoantes a partir " +
+                "de uma string");
+
+            //Q2
+            Console.WriteLine("Q2 - Inverta a ordem dos caracteres de uma string");
+
+            //Q3
+            Console.WriteLine(@"Q3 - Determine se uma string é um palíndromo " +
+                "(se ela é igual quando lida da direita para esquerda e vice-versa)");
+
+            //Q4
+            Console.WriteLine(@"Q4 - Determine se duas strings são anagramas " +
+                "(possuem os mesmos caracteres em ordens diferentes)");
+
+            //Q5
+            Console.WriteLine("Q5 - Valide se a senha inserida atende aos requisitos");
+
+        }
+    }
+}
